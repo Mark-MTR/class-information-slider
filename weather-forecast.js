6fc2324e308c223440606984ef5488af844602d8
Mark-MTR <101233765+Mark-MTR@users.noreply.github.com>
2024-09-08 12:45:19 +0800

    Update weather-forecast.js

--- weather-forecast.js
+++ weather-forecast.js
@@ -22,55 +22,79 @@ function loadForecastData() {
         }
 
         // 显示子信息
-        WF_lg.hoursweather.forEach(hour=>{
-            const subForecastDiv = document.createElement('div');
+        WF_lg.hoursweather.slice(0, 12).forEach(hour => {
+			const subForecastDiv = document.createElement('div');
 
             // 根据类型选择图标
             let logo = '';
+			let color = '';
+			console.log(hour.icon)
             if (hour.icon === '01.png') {
-                logo = '100';
+                logo = '100-fill';
+				color = 'bright';
 			} else if (hour.icon === '02.png') {
-                logo = '102';
+                logo = '102-fill';
+				color = 'bright';
 			} else if (hour.icon === '02_2.png') {
-                logo = '101';
+                logo = '101-fill';
+				color = 'bright';
 			} else if (hour.icon === '03.png') {
-                logo = '104';
+                logo = '104-fill';
+				color = 'cloud';
 			} else if (hour.icon === '04.png') {
-                logo = '300';
+                logo = '300-fill';
+				color = 'rain';
 			} else if (hour.icon === '05.png') {
-                logo = '302';
+                logo = '302-fill';
+				color = 'rain';
 			} else if ((hour.icon === '06.png')||(hour.icon === '08.png')) {
-                logo = '306';
+                logo = '306-fill';
+				color = 'rain';
 			} else if (hour.icon === '07.png') {
-			    logo = '305';
+			    logo = '305-fill';
+				color = 'rain';
             } else if (hour.icon === '09.png') {
-			    logo = '307';
+			    logo = '307-fill';
+				color = 'rain';
 			} else if (hour.icon === '10.png') {
-			    logo = '308';
+			    logo = '308-fill';
+				color = 'rain';
 			} else if (hour.icon === '11.png') {
-			    logo = '150';
+			    logo = '150-fill';
+				color = 'dark';
 			} else if (hour.icon === '12.png') {
-			    logo = '151';
+			    logo = '152-fill';
+				color = 'dark';
+			} else if (hour.icon === '12_2.png') {
+			    logo = '151-fill';
+				color = 'dark';
 			} else if (hour.icon === '13.png') {
-			    logo = '350';
+			    logo = '350-fill';
+				color = 'rain';
 			} else if (hour.icon === '14.png') {
-			    logo = '503';
-			} else if (hour.icon === '14.png') {
-			    logo = '499';
+			    logo = '503-fill';
+				color = 'bright';
+			} else if (hour.icon === '15.png') {
+			    logo = '499-fill';
+				color = 'cloud';
 			} else if (hour.icon === '16.png') {
 			    logo = '1601';
-			} else if (hour.icon === '16.png') {
+				color = 'red';
+			} else if (hour.icon === '17.png') {
 			    logo = '1607';
-			} else if (hour.icon === '16.png') {
-			    logo = '408';
+				color = 'blue';
+			} else if (hour.icon === '18.png') {
+			    logo = '408-fill';
+				color = 'cloud';
 			} else {
-				logo = '999';
+				logo = '999-fill';
+				color = 'cloud';
 			}
 
             subForecastDiv.innerHTML = `
 			<div class="forecast" style="margin-bottom: 10px">
                 <p class="alarm-date">${hour.forecasttime}</p>
-				<p class="forecast-main"><span class="qi-${logo}"></span>&nbsp;${hour.report}</p>
+				<p class="forecast-main"><span class="${color} qi-${logo}"></span>&nbsp;${hour.report}</p>
 				<p class="alarm-date">${hour.maxt}℃&nbsp;${hour.wd}风&nbsp;${hour.wspeed}m/s</p>
 				</div>
             </div>
